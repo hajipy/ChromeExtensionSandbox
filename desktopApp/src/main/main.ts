b0d75a069be141561b673034100305eab8d9bb7d
Kenichi Saito <hajime@studiohff.net>
2018-08-24 02:14:00 +0900

    Electron+Vueのアプリを構築

---
+++ desktopApp/src/main/main.ts
@@ -0,0 +1,38 @@
+import * as path from "path";
+import * as url from "url";
+
+import { app, BrowserWindow } from "electron";
+
+let window: BrowserWindow | null;
+
+function createWindow() {
+    window = new BrowserWindow({ width: 800, height: 600 });
+
+    window.loadURL(url.format({
+        pathname: path.join(__dirname, "../../static/index.html"),
+        protocol: "file:",
+        slashes: true
+    }));
+
+    window.webContents.openDevTools();
+
+    window.on("closed", () => {
+        window = null;
+    });
+}
+
+app.on("ready", () => {
+    createWindow();
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("activate", () => {
+    if (window === null) {
+        createWindow();
+    }
+});
---
+++ desktopApp/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+
+const moduleSetting = {
+    rules: [
+        {
+            test: /\.ts$/,
+            loader: "ts-loader",
+            options: {
+                appendTsSuffixTo: [/\.vue$/]
+            }
+        },
+        {
+            test: /\.vue$/,
+            loader: "vue-loader",
+        }
+    ]
+};
+
+const resolveSetting = {
+    extensions: [".js", ".ts"],
+    alias: {
+        "vue$": "vue/dist/vue.esm.js",
+        "vuex$": "vuex/dist/vuex.esm.js"
+    }
+};
+
+var nodeSetting = {
+    __dirname: false,
+};
+
+const mainProcessConfig = {
+    entry: "./src/main/main.ts",
+    output: {
+        path: path.resolve(__dirname, "dist/main"),
+        filename: "bundle.js"
+    },
+    target: "electron-main",
+    module: moduleSetting,
+    resolve: resolveSetting,
+    node: nodeSetting
+};
+
+const rendererProsessConfig = {
+    entry: "./src/renderer/main.ts",
+    output: {
+        path: path.resolve(__dirname, "dist/renderer"),
+        filename: "bundle.js"
+    },
+    target: "electron-renderer",
+    module: moduleSetting,
+    resolve: resolveSetting
+};
+
+module.exports = [
+    mainProcessConfig, rendererProsessConfig
+];
