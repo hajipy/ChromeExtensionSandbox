8b84347e58dcade2ad6e6e6949f349b8da25360f
Kenichi Saito <hajime@studiohff.net>
2018-08-25 17:27:17 +0900

    コンテキストメニューからURLを送る

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,20 @@
-function sendHttpRequest() {
+function sendHttpRequest(url) {
+    fetch("http://127.0.0.1:53737/", {
+        body: JSON.stringify({ url }),
+        cache: "no-cache",
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "origin": "http://example.com"
+        },
+        mode: "cors",
+    })
+        .then(function(response) {
+            console.log(response);
+        });
+}
+
+function browserActionHandler() {
     chrome.tabs.query({ "active": true, "lastFocusedWindow": true }, (tabs) => {
         console.log(tabs);
 
@@ -8,20 +24,44 @@ function sendHttpRequest() {
 
         const url = tabs[0].url;
 
-        fetch("http://127.0.0.1:53737/", {
-            body: JSON.stringify({ url }),
-            cache: "no-cache",
-            method: "POST",
-            headers: {
-                "content-type": "application/json",
-                "origin": "http://example.com"
-            },
-            mode: "cors",
-        })
-        .then(function(response) {
-            console.log(response);
-        });
+        sendHttpRequest(url);
     });
 }
 
-chrome.browserAction.onClicked.addListener(sendHttpRequest);
+function pageContextMenuHandler(info) {
+    if (info.pageUrl) {
+        sendHttpRequest(info.pageUrl);
+    }
+}
+
+function imageContextMenuHandler(info) {
+    if (info.srcUrl) {
+        sendHttpRequest(info.srcUrl);
+    }
+}
+
+function linkContextMenuHandler(info) {
+    if (info.linkUrl) {
+        sendHttpRequest(info.linkUrl);
+    }
+}
+
+chrome.browserAction.onClicked.addListener(browserActionHandler);
+
+chrome.contextMenus.create({
+    title: "このページのURLを送る",
+    contexts: ["page"],
+    onclick: pageContextMenuHandler
+});
+
+chrome.contextMenus.create({
+    title: "画像のURLを送る",
+    contexts: ["image"],
+    onclick: imageContextMenuHandler,
+});
+
+chrome.contextMenus.create({
+    title: "リンク先のURLを送る",
+    contexts: ["link"],
+    onclick: linkContextMenuHandler,
+});
