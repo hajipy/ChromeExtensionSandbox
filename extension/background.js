323b59719a60bc1323b9ca37f2e07b0cb5d636b7
Kenichi Saito <hajime@studiohff.net>
2018-08-25 16:19:21 +0900

    ブラウザ拡張をクリックするとWebサーバにデータを送る

--- extension/background.js
+++ extension/background.js
@@ -1,15 +1,17 @@
-function sendNativeMessage() {
-    const port = chrome.runtime.connectNative("net.hajipy.sandbox");
-
-    port.onMessage.addListener((message) => {
-        console.log("receive " + JSON.stringify(message, null, 0));
+function sendHttpRequest() {
+    fetch("http://127.0.0.1:53737/", {
+        body: JSON.stringify({ url: "http://example.com" }),
+        cache: "no-cache",
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "origin": "http://example.com"
+        },
+        mode: "cors",
+    })
+    .then(function(response) {
+        console.log(response);
     });
-
-    port.onDisconnect.addListener(() => {
-        console.log("disconnected");
-    });
-
-    port.postMessage({ text: "hello from chrome extension" });
 }
 
-chrome.browserAction.onClicked.addListener(sendNativeMessage);
+chrome.browserAction.onClicked.addListener(sendHttpRequest);
