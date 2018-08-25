5824e22a4c17d22b66aa2ecffd8bf34bdf04153c
Kenichi Saito <hajime@studiohff.net>
2018-08-25 14:33:14 +0900

    rendererプロセスは受け取ったリクエストを表示する

--- desktopApp/src/renderer/main.ts
+++ desktopApp/src/renderer/main.ts
@@ -1,14 +1,37 @@
 import { ipcRenderer } from "electron";
 
 import Vue from "vue";
+import Vuex, { MutationTree, mapState } from "vuex";
+
+Vue.use(Vuex);
+
+class State {
+    public urls: string[] = [];
+}
+
+const mutations: MutationTree<State> = {
+    addUrl(state, url: string) {
+        state.urls.push(url);
+    },
+};
+
+const store = new Vuex.Store({
+    state: new State(),
+    mutations,
+});
 
 const app = new Vue({
     el: "#app",
-    data: {
-        message: "hello from vue"
-    }
+    store,
+    computed: {
+        ...mapState([
+            "urls",
+        ]),
+    },
 });
 
 ipcRenderer.on("add-url", (event: any, payload: { url: string }) => {
     console.log(`add-url: ${JSON.stringify(payload, null, 4)}`);
+
+    app.$store.commit("addUrl", payload.url);
 });
