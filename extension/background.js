e8d1dd9dcac7f8d3a280a136a08a827371b5767a
Kenichi Saito <hajime@studiohff.net>
2018-08-25 16:41:34 +0900

    アクティブなタブのURLを送る

--- extension/background.js
+++ extension/background.js
@@ -1,16 +1,26 @@
 function sendHttpRequest() {
-    fetch("http://127.0.0.1:53737/", {
-        body: JSON.stringify({ url: "http://example.com" }),
-        cache: "no-cache",
-        method: "POST",
-        headers: {
-            "content-type": "application/json",
-            "origin": "http://example.com"
-        },
-        mode: "cors",
-    })
-    .then(function(response) {
-        console.log(response);
+    chrome.tabs.query({ "active": true, "lastFocusedWindow": true }, (tabs) => {
+        console.log(tabs);
+
+        if (tabs.length === 0) {
+            return;
+        }
+
+        const url = tabs[0].url;
+
+        fetch("http://127.0.0.1:53737/", {
+            body: JSON.stringify({ url }),
+            cache: "no-cache",
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+                "origin": "http://example.com"
+            },
+            mode: "cors",
+        })
+        .then(function(response) {
+            console.log(response);
+        });
     });
 }
 
